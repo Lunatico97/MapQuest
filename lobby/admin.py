36a7088fa2b5247db9952f48e7a3bd549fde4fd9
Lunatico97 <lunatico123@gmail.com>
2024-02-07 14:20:22 +0545

    Added few table

--- lobby/admin.py
+++ lobby/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Participant
+from .models import Lobby, Participant
 
 # Register your models here.
+admin.site.register(Lobby) ;
 admin.site.register(Participant) ;
--- lobby/models.py
+++ lobby/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Lobby(models.Model):
+	name = models.CharField(max_length=15) ;
+	secret_key = models.CharField(max_length=6) ;
+	purpose = models.TextField(max_length=200, default='') ;
+	
+	def __str__(self):
+		return self.name ;
+
 class Participant(models.Model):
 	name = models.CharField(max_length=30) ;
 	age = models.IntegerField() ;
 	bio = models.TextField(max_length=200, default='') ;
+	lobby = models.ForeignKey(Lobby, on_delete=models.CASCADE) ;
 	
 	def __str__(self):
 		return self.name ;
